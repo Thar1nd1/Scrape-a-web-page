9ddd49d836a1e427849c364c9087663c98cd5622
Thar1nd1 <tharindijayamanna@gmail.com>
2024-01-03 23:38:24 +0530

    These are initial files

---
+++ taylorWikiScrape.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://en.wikipedia.org/wiki/Taylor_Swift"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+paragraphs = soup.find_all('p')
+
+for paragraph in paragraphs:
+    print(paragraph.get_text())
